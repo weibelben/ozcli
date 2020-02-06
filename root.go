5d9cb39cb1bcdf7155c1cf0de847a91c2fd9ab7e
Ben Weibel <33881712+weibelben@users.noreply.github.com>
2020-02-05 17:49:02 -0800

    Create root.go

---
+++ root.go
@@ -0,0 +1,40 @@
+package ozcli
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ozcli",
+	Short: "OzCLI is your portal to Oz",
+	Long: `A command driven interface for launching, modifying, and destroying
+                Oz environments and related services.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func init() {
+	cobra.OnInitialize(setWorkingDirectory)
+}
+
+func setWorkingDirectory() {
+	path := "."
+	if err := os.Chdir(path); err != nil {
+		log.WithError(err).Fatalf(
+			"could not set ozcli's working directory to %s", path)
+	}
+}
+
+// Execute is the main entry point into the Cobra cli. It parses the options
+// provided and executes the desired functions based on commands and flags.
+func Execute() {
+	wd, _ := os.Getwd()
+	defer func() { _ = os.Chdir(wd) }()
+	if err := rootCmd.Execute(); err != nil {
+		log.WithError(err).Fatal("Failed to run ozcli")
+	}
+}
