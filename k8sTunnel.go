0c0b80c72d698892d261256da4e2d9a1f593100b
Ben Weibel <33881712+weibelben@users.noreply.github.com>
2020-02-11 09:09:37 -0800

    Update k8sTunnel.go

--- k8sTunnel.go
+++ k8sTunnel.go
@@ -27,6 +27,6 @@ func createK8sTunnel() {
 	err := makeTunnelCmd.Run()
 	log.Info("tunneling...")
 	if err != nil {
-		log.WithError(err).Error("Failed to create tunnel to k8s api.")
+		log.WithError(err).Info("Failed to create tunnel to k8s api.")
 	}
 }
